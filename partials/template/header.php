139389548bf928513a5d16a377b2131d028d36d9
Marco-Gagliano <brazil_94@hotmail.it>
2022-06-09 15:32:34 +0200

    preparazione-pagina

---
+++ partials/template/header.php
@@ -0,0 +1,8 @@
+<?php 
+
+// per raggiungere text-php e vars.php devo uscire dalla mia cartella e raggiungere i file, quindi dopo DIR devo fare /../
+
+  echo __DIR__ . '/../vars.php';
+  echo __DIR__ . '/../text.php';
+
+?>
---
+++ partials/vars.php
@@ -0,0 +1,6 @@
+<?php
+
+  $name = 'Pippo';
+  $color = 'green'
+
+?>
